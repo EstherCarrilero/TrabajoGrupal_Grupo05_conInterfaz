c1e8b0df106be15bbaa725fd7e568829bf3688bb
luciaapeerez <luciaperezabad2004@gmail.com>
2023-04-06 12:34:47 +0200

    BOTÓN DE CONTAR PAISES CORRECTO

--- src/Paginas/MainFrame.java
+++ src/Paginas/MainFrame.java
@@ -44,10 +44,10 @@ public class MainFrame extends JFrame{
     private JLabel etiLogo;
     private JLabel etiError;
 
-    public MainFrame(){
+    public MainFrame() {
         setContentPane(mainPanel);
         setTitle("Gestión de contadores");
-        setSize(1300,650);
+        setSize(1300, 650);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
@@ -59,46 +59,72 @@ public class MainFrame extends JFrame{
 
         //---BOTON MOSTRAR DATOS---
         //Ejemplo para probar
-        Hub h1=new Hub();
-        Contenedor c1=new Contenedor(100,1500,"España",true,3,"Nada","Correos Express","Alireparte");
-        Contenedor c2=new Contenedor(101,1501,"España",false,2,"Nada 2","Correos Express","Alireparte");
+        Hub h1 = new Hub();
+        Contenedor c1 = new Contenedor(100, 1500, "España", true, 3, "Nada", "Correos Express", "Alireparte");
+        Contenedor c2 = new Contenedor(101, 1501, "España", false, 2, "Nada 2", "Correos Express", "Alireparte");
         h1.apilar(c1);
         h1.apilar(c2);
         botMostrarDatos.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Contenedor resultado;
-                Contenedor correcto=null;
-                for (int i=0;i<10;i++){
-                    for (int j=0;j<12;j++){
-                        if(h1.getM(i,j)!=null){
-                            resultado=h1.getM(i,j);
-                            if(resultado.getId()==Integer.parseInt(textMostrarDatos.getText())){
-                                correcto=h1.getM(i,j);
+                Contenedor correcto = null;
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < 12; j++) {
+                        if (h1.getM(i, j) != null) {
+                            resultado = h1.getM(i, j);
+                            if (resultado.getId() == Integer.parseInt(textMostrarDatos.getText())) {
+                                correcto = h1.getM(i, j);
                                 break;
                             }
                         }
                     }
                 }
-                if (correcto!=null) {
+                if (correcto != null) {
                     etiError.setText("");
                     Pag2 verPag2 = new Pag2(correcto);
                     verPag2.setVisible(true);
-                }else{
+                } else {
                     etiError.setText("* Error número de identificador equivocado");
                 }
             }
         });
-
+        //BOTON DE CUANTOS
+        botCuantos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Contenedor c;
+                String itemSeleccionado = (String) comboBoxCuantos.getSelectedItem();
+                int contador = 0;
+                String s = " ";
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < 12; j++) {
+                        if (h1.getM(i, j) != null) {
+                            c = h1.getM(i, j);
+                            if(c.getPais()==itemSeleccionado){
+                                contador = contador + 1;
+                            }
+                        }
+                    }
+                }
+                if (contador!=0) {
+                    s = s + contador;
+                    textCuantos.setText(s);
+                } else {
+                    s = s + 0;
+                    textCuantos.setText(s);
+                }
+            }
+        });
         //---CONTROLA NUMERO DE IDENTIFICACION---
         textNumIdentificacion.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
                 char caracter = e.getKeyChar();
 
-                if(((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')){
+                if (((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')) {
                     e.consume();
-                    JOptionPane.showMessageDialog(null,"El dato no es numérico, vuelve a introducirlo");
+                    JOptionPane.showMessageDialog(null, "El dato no es numérico, vuelve a introducirlo");
                 }
             }
         });
@@ -109,9 +135,9 @@ public class MainFrame extends JFrame{
             public void keyTyped(KeyEvent e) {
                 char caracter = e.getKeyChar();
 
-                if(((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')){
+                if (((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')) {
                     e.consume();
-                    JOptionPane.showMessageDialog(null,"El dato no es numérico, vuelve a introducirlo");
+                    JOptionPane.showMessageDialog(null, "El dato no es numérico, vuelve a introducirlo");
                 }
             }
         });
@@ -120,7 +146,7 @@ public class MainFrame extends JFrame{
         textDescContent.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                if(textDescContent.getText().length() >= 100){
+                if (textDescContent.getText().length() >= 100) {
                     e.consume();
                 }
             }
@@ -130,7 +156,7 @@ public class MainFrame extends JFrame{
         textEmpRemitente.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                if(textEmpRemitente.getText().length() >= 20){
+                if (textEmpRemitente.getText().length() >= 20) {
                     e.consume();
                 }
             }
@@ -140,7 +166,7 @@ public class MainFrame extends JFrame{
         textEmpReceptora.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                if(textEmpReceptora.getText().length() >= 20){
+                if (textEmpReceptora.getText().length() >= 20) {
                     e.consume();
                 }
             }
@@ -152,9 +178,9 @@ public class MainFrame extends JFrame{
             public void keyTyped(KeyEvent e) {
                 char caracter = e.getKeyChar();
 
-                if(((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')){
+                if (((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')) {
                     e.consume();
-                    JOptionPane.showMessageDialog(null,"El dato no es numérico, vuelve a introducirlo");
+                    JOptionPane.showMessageDialog(null, "El dato no es numérico, vuelve a introducirlo");
                 }
             }
         });
@@ -165,13 +191,16 @@ public class MainFrame extends JFrame{
             public void keyTyped(KeyEvent e) {
                 char caracter = e.getKeyChar();
 
-                if(((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')){
+                if (((caracter < '0') || (caracter > '9')) && (caracter != '\b') && (caracter != '\n')) {
                     e.consume();
-                    JOptionPane.showMessageDialog(null,"El dato no es numérico, vuelve a introducirlo");
+                    JOptionPane.showMessageDialog(null, "El dato no es numérico, vuelve a introducirlo");
                 }
             }
         });
+
     }
+        //BOTÓN DE CONTAR PAISES
+
 
     public static void main(String[] args) {
         MainFrame pag1 = new MainFrame();
